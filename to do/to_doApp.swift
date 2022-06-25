822362d223e60c98a828a941dcd87f2155d20264
YapTong <chanyaptong@gmail.com>
2022-06-25 10:15:03 +0800

    Initial Commit

---
+++ to do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to do
+//
+//  Created by Chan Yap Tong on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
